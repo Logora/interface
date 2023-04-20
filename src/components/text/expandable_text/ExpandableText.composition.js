a740fb624af931448d7ed2889118c18896234573
Mboulgour <mboulgour@protonmail.com>
2023-04-20 11:23:01 +0200

    Add ExpandableText component

---
+++ src/components/text/expandable_text/ExpandableText.composition.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ExpandableText } from './ExpandableText';
+import { faker } from '@faker-js/faker';
+
+let text = faker.lorem.paragraph(35);
+
+export const DefaultExpandableText = () => {
+    return (
+        <ExpandableText 
+            expandable
+            expandText={"Read more"}
+            collapseText={"Read less"}
+            maxHeight='100'
+        >
+            <div>
+                {text}
+            </div>
+        </ExpandableText>
+    )
+}
+
+export const DefaultDarkBackgroundExpandableText = () => {
+    return (
+        <div style={{ background: "#000", padding: "50px" }}>
+            <ExpandableText 
+                expandable
+                expandText={"Read more"}
+                collapseText={"Read less"}
+                maxHeight='100'
+                backgroundColor='#000'
+            >
+                <div style={{ color: "#FFF" }}>
+                    {text}
+                </div>
+            </ExpandableText>
+        </div>
+    )
+}
---
+++ src/components/text/expandable_text/ExpandableText.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { ArrowIcon } from '@logora/debate.icons';
+import styles from './ExpandableText.module.scss';
+import cx from 'classnames';
+
+export const ExpandableText = (
+    { 
+        children, 
+        expandable = true, 
+        expandText = "Read more", 
+        collapseText = "Read less", 
+        className,
+        maxHeight = "100", 
+        showIcon = true,
+        backgroundColor = "#ffffff",
+        onCollapse = () => {}, 
+        onExpand = () => {}, 
+    }
+) => {
+	const [isExpanded, setIsExpanded] = useState(false);
+	const [contentHeight, setContentHeight] = useState(0);
+	const contentRef = useRef(null);
+
+	useEffect(() => {
+		if(expandable) {
+			setContentHeight(getContentHeight());
+		}
+	}, []);
+
+	const getContentHeight = () => {
+		const heightPx = contentRef.current.clientHeight;
+		return Math.round(heightPx);
+	};
+
+	const handleCollapse = () => {
+		setIsExpanded(false);
+		onCollapse();
+	};
+
+	const handleExpand = () => {
+		setIsExpanded(true);
+		onExpand();
+	};
+
+    const shouldShowButton = expandable && (contentHeight > maxHeight);
+    const contentStyles = {
+        maxHeight: maxHeight + "px",
+    }
+	const maskStyles = {
+		background: `linear-gradient(to bottom, rgba(255, 255, 255, 0), ${backgroundColor} 90%)`
+	}
+
+    return (
+        <div className={styles.container} ref={contentRef}>
+            <div 
+                style={ shouldShowButton && !isExpanded ? contentStyles : {}} 
+                className={cx({ [styles.contentMask]: shouldShowButton && !isExpanded })}
+            >
+                { children }
+            </div>
+			{ shouldShowButton &&
+				( !isExpanded ?
+					<div 
+						style={ shouldShowButton && !isExpanded ? maskStyles : {}} 
+						className={styles.linkContainer} 
+						onClick={() => handleExpand()}
+					>
+						<div className={cx(styles.link, className)}>
+							<span>{expandText}</span>
+                            { showIcon && <ArrowIcon height={16} width={16} /> }
+						</div>
+					</div> 
+					:
+					<div className={styles.linkContainer} onClick={() => handleCollapse()}>
+						<div className={cx(styles.linkExpanded, className)}>
+							<span>{collapseText}</span>
+                            { showIcon && <ArrowIcon height={16} width={16} /> }
+						</div>
+					</div>
+				)
+			}
+        </div>
+    )
+}
