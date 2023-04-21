b55f3cd92b86d171e340f271d200cdcfdd0c577b
Neko-cat <mathilde.couvreur@gmail.com>
2023-04-21 12:01:57 +0200

    Add ScrollToTop component

---
+++ src/components/tools/scroll_to_top/ScrollToTop.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const ScrollToTop = (props) => {
+    const { pathname, search } = useLocation();
+
+    useEffect(() => {
+        if(typeof window !== 'undefined') {
+            const element = document.getElementById(props.elementId);
+            if (element) {
+                element.scrollIntoView();
+            }
+        }
+    }, [pathname, search]);
+
+    return null;
+}
---
+++ src/components/tools/scroll_to_top/ScrollToTop.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ScrollToTop } from './ScrollToTop';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('ScrollToTop', () => {  
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should scroll to the element with the specified ID', async () => {
+        // Mock the window and its properties
+        const scrollIntoViewMock = jest.fn();
+        const getElementByIdMock = jest.fn(() => ({
+            scrollIntoView: scrollIntoViewMock
+        }));
+        window.document.getElementById = getElementByIdMock;
+        window.scrollTo = jest.fn();
+
+        // Render the component with a dummy element
+        render(
+            <BrowserRouter>
+                <ScrollToTop elementId="my-element" />
+                <div id="my-element">Dummy element</div>
+            </BrowserRouter>
+        );
+
+        // Expect that the DOM methods were called as expected
+        expect(getElementByIdMock).toHaveBeenCalledWith('my-element');
+        expect(scrollIntoViewMock).toHaveBeenCalled();
+    });
+
+    it('should not scroll when the location changes and an element ID is provided, but the element is not found', () => {
+        const getElementByIdMock = jest.fn(() => null);
+        jest.spyOn(document, 'getElementById').mockImplementation(getElementByIdMock);
+    
+        const { rerender } = render(
+          <BrowserRouter>
+            <ScrollToTop elementId="non-existent-element" />
+          </BrowserRouter>
+        );
+    
+        // Expect that getElementById and scrollIntoView were not called
+        expect(getElementByIdMock).toHaveBeenCalledTimes(1);
+        expect(getElementByIdMock).toHaveBeenCalledWith('non-existent-element');
+        expect(getElementByIdMock.mock.results[0].value).toBeNull();
+    });       
+});
