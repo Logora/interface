5850cc3d851e1992e42c374c857d91a61a5afa11
Henry Boisgibault <h.boisgibault@gmail.com>
2023-09-26 18:42:53 +0200

    Added PropTypes to Button & Link

--- src/components/action/link/Link.jsx
+++ src/components/action/link/Link.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import PropTypes from "prop-types";
 
-export const Link = (props) => {
-    const { className, to, external, rel, target, children, ...rest } = props;
-
+export const Link = ({ className, to, external, rel = "nofollow", target = "_self", children, ...rest }) => {
     if(external) {
         return (
             <a 
                 href={to} 
                 className={className} 
                 role="link"
-                target={target ? target : "_self"} 
-                rel={rel ? rel : "nofollow"} 
+                target={target} 
+                rel={rel} 
                 {...rest}
             >
                 { children }
@@ -23,8 +22,8 @@ export const Link = (props) => {
                 to={to} 
                 role="link"
                 className={className} 
-                target={target ? target : "_self"} 
-                rel={rel ? rel : "nofollow"} 
+                target={target} 
+                rel={rel} 
                 {...rest}
             >
                 { children }
@@ -32,3 +31,20 @@ export const Link = (props) => {
         );
     }
 }
+
+Link.propTypes = {
+    /** href, or object passed to react-router */
+    to: PropTypes.any,
+    /** Class name to style the link */
+    className: PropTypes.string,
+    /** If `true`, will render a plain anchor tag instead of react-router's Link */
+    external: PropTypes.bool,
+    /** rel HTML attribute  */
+    rel: PropTypes.string,
+    /**  target HTML attribute */
+    target: PropTypes.string,
+    /**  Content of the link */
+    children: PropTypes.node,
+    /** Extra props passed to the component */
+    rest: PropTypes.object
+  };
