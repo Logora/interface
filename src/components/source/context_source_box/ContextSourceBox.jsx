7daa120a93da91ac2fc88eaaacc3d5b72aa63806
Henry Boisgibault <h.boisgibault@gmail.com>
2023-09-29 09:22:01 +0200

    Add ContextSourceBox component and its dependencies

---
+++ src/components/source/context_source_box/ContextSourceBox.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FormattedDate } from 'react-intl';
+import styles from './ContextSourceBox.module.scss';
+import PropTypes from "prop-types";
+
+export const ContextSourceBox = ({ title, imageUrl, date, author }) => {
+  return (
+    <div className={styles.container}>
+        <img src={imageUrl} alt="" className={styles.image} />
+        <div className={styles.content}>
+            <div className={styles.infos}>
+              { author &&
+                <>
+                  <span className={styles.sourceAuthor}>{author}</span>
+                  <span className={styles.separator}>•</span>
+                </>
+              }
+              <span className={styles.sourcePublishedDate}><FormattedDate value={new Date(date)} year="numeric" month="long" day="2-digit" /></span>
+            </div>
+            <div className={styles.sourceTitle}>{title}</div>
+        </div>
+    </div>
+  )
+}
+
+ContextSourceBox.propTypes = {
+  /** Title of the source */
+  title: PropTypes.string.isRequired,
+  /** Author of the source */
+  author: PropTypes.string,
+  /** Publication date of the source */
+  date: PropTypes.string,
+  /**  URL of the source image */
+  imageUrl: PropTypes.string
+};
