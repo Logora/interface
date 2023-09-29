d6ffd239d01f95776e655f49e049683bbbb73cf5
Henry Boisgibault <h.boisgibault@gmail.com>
2023-09-29 15:20:45 +0200

    ProgressBar component

---
+++ src/components/progress/progress_bar/ProgressBar.test.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ProgressBar } from './ProgressBar';
+
+describe('ProgressBar', () => {
+    it('should render progressBar with children', () => {
+        const progressBar = render(
+            <ProgressBar 
+                progress={25} 
+                goal={100}
+                className={"myClass"} 
+                innerClassName={"myInnerClass"}
+                subtitleClassName={"subtitleClassName"}
+            >
+                My progress bar
+            </ProgressBar>
+        );
+
+        expect(screen.getByText("My progress bar")).toBeTruthy();
+        expect(screen.getByRole("progress")).toBeTruthy();
+    });
+
+    it('should render progressBar with title', () => {
+        const progressBar = render(
+            <ProgressBar 
+                progress={25} 
+                goal={100} 
+                title={"Title"}
+            >
+                My progress bar
+            </ProgressBar>
+        );
+
+        expect(screen.getByText("Title")).toBeTruthy();
+        expect(screen.getByText("My progress bar")).toBeTruthy();
+        expect(screen.getByRole("progress")).toBeTruthy();
+    });
+
+    it('should render progressBar with progress subtitle', () => {
+        const progressBar = render(
+            <ProgressBar 
+                progress={25} 
+                goal={100} 
+                showProgressSubtitle={true}
+                progressUnit={"votes"}
+            />
+        );
+
+        expect(screen.getByText("25 / 100 votes (25%)")).toBeTruthy();
+        expect(screen.getByRole("progress")).toBeTruthy();
+    });
+
+    it('should render progressBar with progress subtitle without unit', () => {
+        const progressBar = render(
+            <ProgressBar 
+                progress={25} 
+                goal={100} 
+                showProgressSubtitle={true}
+            />
+        );
+
+        expect(screen.getByText("25 / 100 (25%)")).toBeTruthy();
+        expect(screen.getByRole("progress")).toBeTruthy();
+    });
+
+    it('should render progressBar with full progress subtitle', () => {
+        const progressBar = render(
+            <ProgressBar 
+                progress={25} 
+                goal={100} 
+                showProgressSubtitle={true}
+                barFull={true}
+                progressUnit={"votes"}
+            />
+        );
+
+        expect(screen.getByText("25 votes")).toBeTruthy();
+        expect(screen.getByRole("progress")).toBeTruthy();
+    });
+
+    it('should render progressBar with percentage subtitle', () => {
+        const progressBar = render(
+            <ProgressBar 
+                progress={25} 
+                goal={100} 
+                showPercentageSubtitle={true}
+            />
+        );
+
+        expect(screen.getByText("25%")).toBeTruthy();
+        expect(screen.getByRole("progress")).toBeTruthy();
+    });
+
+    it('should render progressBar even if goal is 0', () => {
+        const progressBar = render(
+            <ProgressBar 
+                progress={25} 
+                goal={0} 
+                title={"Title"}
+                showPercentageSubtitle={true}
+            >
+                My progress bar
+            </ProgressBar>
+        );
+
+        expect(screen.getByText("Title")).toBeTruthy();
+        expect(screen.getByText("My progress bar")).toBeTruthy();
+        expect(screen.getByRole("progress")).toBeTruthy();
+        expect(screen.getByText("0%")).toBeTruthy();
+    });
+});
---
+++ src/components/tools/progress_bar/ProgressBar.composition.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { ProgressBar } from "./ProgressBar";
+
+export const DefaultProgressBar = () => {
+    return (
+        <ProgressBar 
+            progress={25} 
+            goal={100}
+        >
+            Nice progress !
+        </ProgressBar>
+    )
+}
+
+export const ProgressBarWithTitle = () => {
+    return (
+        <ProgressBar 
+            progress={25} 
+            goal={100}
+            title="Progress title"
+        >
+            Nice progress !
+        </ProgressBar>
+    )
+}
+
+export const ProgressBarWithPercentageSubtitle = () => {
+    return (
+        <ProgressBar 
+            progress={25} 
+            goal={100}
+            title="Progress title"
+            showPercentageSubtitle={true}
+        >
+            Nice progress !
+        </ProgressBar>
+    )
+}
+
+export const ProgressBarWithProgressSubtitle = () => {
+    return (
+        <ProgressBar 
+            progress={25} 
+            goal={100}
+            title="Progress title"
+            showProgressSubtitle={true}
+            progressUnit={"votes"}
+        >
+            Nice progress !
+        </ProgressBar>
+    )
+}
+
+
+export const ProgressBarWithProgressSubtitleLeft = () => {
+    return (
+        <ProgressBar 
+            progress={25} 
+            goal={100}
+            title="Progress title"
+            showProgressSubtitle={true}
+            progressUnit={"votes"}
+            isLeft={true}
+        >
+            Nice progress !
+        </ProgressBar>
+    )
+}
+
+export const ProgressBarWithProgressSubtitleFullBar = () => {
+    return (
+        <ProgressBar 
+            progress={100} 
+            goal={100}
+            title="Progress title"
+            showProgressSubtitle={true}
+            barFull={true}
+            progressUnit={"votes"}
+        >
+            Nice progress !
+        </ProgressBar>
+    )
+}
