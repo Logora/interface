d4dd202522464b15bb5e037c655930639bcbc859
Mboulgour <mboulgour@protonmail.com>
2023-10-02 17:48:24 +0200

    🩹 Remove ModalProvider from AvatarSelector comp

--- src/components/user/avatar_selector/AvatarSelector.composition.js
+++ src/components/user/avatar_selector/AvatarSelector.composition.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { AvatarSelector } from "./AvatarSelector";
 import { IntlProvider } from "react-intl";
-import { ModalProvider } from "@logora/debate.dialog.modal";
 import { faker } from '@faker-js/faker';
 
 const avatarUrlList = [faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar(), faker.image.avatar()];
@@ -13,15 +12,13 @@ const handleChooseAvatar = (avatar) => {
 export const DefaultAvatarSelector = () => {
     return (
         <div style={{width:"500px", height: "300px"}}>
-            <ModalProvider>
-                <IntlProvider locale="en">
-                    <AvatarSelector
-                        onChooseAvatar={handleChooseAvatar}
-                        avatarUrlList={avatarUrlList}
-                        userName={"User Name"}
-                    />
-                </IntlProvider>
-            </ModalProvider>
+            <IntlProvider locale="en">
+                <AvatarSelector
+                    onChooseAvatar={handleChooseAvatar}
+                    avatarUrlList={avatarUrlList}
+                    userName={"User Name"}
+                />
+            </IntlProvider>
         </div>
     );
 };
@@ -29,16 +26,14 @@ export const DefaultAvatarSelector = () => {
 export const AllowUserImageAvatarSelector = () => {
     return (
         <div style={{width:"500px", height: "300px"}}>
-            <ModalProvider>
-                <IntlProvider locale="en">
-                    <AvatarSelector
-                        onChooseAvatar={handleChooseAvatar}
-                        avatarUrlList={avatarUrlList}
-                        userName={"User Name"}
-                        allowUserImage
-                    />
-                </IntlProvider>
-            </ModalProvider>
+            <IntlProvider locale="en">
+                <AvatarSelector
+                    onChooseAvatar={handleChooseAvatar}
+                    avatarUrlList={avatarUrlList}
+                    userName={"User Name"}
+                    allowUserImage
+                />
+            </IntlProvider>
         </div>
     );
 };
