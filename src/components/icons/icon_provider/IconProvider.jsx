4a13ef1a37380e93d1f727f61cbaf9ec179333c6
Henry Boisgibault <h.boisgibault@gmail.com>
2023-10-12 14:08:59 +0200

    Add new generic icon component and provider

---
+++ src/components/icons/icon_provider/IconProvider.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { IconContext } from './IconContext';
+import PropTypes from "prop-types";
+
+export const IconProvider = ({ libraryName = "regular", children }) => {
+    const [iconLibrary, setIconLibrary] = useState(null);
+
+    const importIconLibrary = async () => {
+        return import(`@logora/debate.icons.${libraryName}_icons`);
+    };
+
+    useEffect(() => {
+        const loadIconLibrary = async () => {
+            try {
+                const iconLibraryModule = await importIconLibrary();
+                setIconLibrary(iconLibraryModule);
+            } catch(e) {
+                setIconLibrary(null);
+            }
+        };
+
+        loadIconLibrary();
+    }, [libraryName]);
+
+    return (
+        <IconContext.Provider value={{ iconLibrary }}>
+            {children}
+        </IconContext.Provider>
+    );
+}
+
+IconProvider.propTypes = {
+    /** Icon library name */
+    libraryName: PropTypes.string.isRequired,
+    /** Provider children */
+	children: PropTypes.node,
+};
+
+IconProvider.defaultProps = {
+    libraryName: "regular"
+}
