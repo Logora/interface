ac1452ba42e679b5bdcf9898d82edbee042db024
Mboulgour <mboulgour@protonmail.com>
2023-10-12 11:49:38 +0200

    ✨ Add new regular library

---
+++ src/components/icons/regular_icons/Code.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const CodeIcon = (props) => (
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M6.147 10.473 3.238 8.187l2.91-2.078M9.784 10.473l2.909-2.286-2.91-2.078"
+      stroke="#000"
+      fill="none"
+    />
+    <path
+      transform="matrix(.27976 -.96007 .92211 .38692 7.22 11.54)"
+      strokeWidth={0.7}
+      stroke="#000"
+      d="M0-.35h6.565"
+    />
+    <rect x={0.5} y={0.5} width={15} height={15} rx={2.5} fill="none" stroke="#000" />
+  </svg>
+)
