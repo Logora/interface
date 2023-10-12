ad79832a45c6be0db173b8c04b70130d4ac0108f
Henry Boisgibault <h.boisgibault@gmail.com>
2023-10-12 16:36:18 +0200

    Refactor IconProvider to remove unused code and improve code readability

--- src/components/icons/icon_provider/IconProvider.jsx
+++ src/components/icons/icon_provider/IconProvider.jsx
@@ -1,33 +1,19 @@
-import React, { useState, useEffect, lazy, Suspense } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IconContext } from './IconContext';
 import PropTypes from "prop-types";
 
 export const IconProvider = ({ libraryName = "regular", children }) => {
     const [iconLibrary, setIconLibrary] = useState(null);
 
-    const importIconLibrary = async () => {
-        if(libraryName === "regular") {
-            return lazy(() => import("@logora/debate.icons.regular_icons"));
-        }
-    };
-
     useEffect(() => {
-        const loadIconLibrary = async () => {
-            try {
-                const iconLibraryModule = await importIconLibrary();
-                setIconLibrary(iconLibraryModule);
-            } catch(e) {
-                setIconLibrary(null);
-            }
-        };
-        loadIconLibrary();
+        (() => import("@logora/debate.icons.regular_icons"))().then((library) => {
+            setIconLibrary(library);
+        });
     }, [libraryName]);
 
     return (
         <IconContext.Provider value={{ iconLibrary }}>
-            <Suspense fallback={null}>
-                { children }
-            </Suspense>
+            { children }
         </IconContext.Provider>
     );
 }
