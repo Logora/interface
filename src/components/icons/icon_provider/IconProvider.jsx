c550d2365d7505489d58487f8763436d4832af4f
Henry Boisgibault <h.boisgibault@gmail.com>
2023-10-13 14:17:06 +0200

    Refactor IconProvider to dynamically import icon libraries

--- src/components/icons/icon_provider/IconProvider.jsx
+++ src/components/icons/icon_provider/IconProvider.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect, lazy } from 'react';
 import { IconContext } from './IconContext';
 import PropTypes from "prop-types";
-const RegularIcons = lazy(() => import('@logora/debate.icons.regular_icons'));
-const SpiegelIcons = lazy(() => import('@logora/debate.icons.spiegel_icons'));
 
 export const IconProvider = ({ libraryName, children }) => {
     const [iconLibrary, setIconLibrary] = useState(null);
 
     useEffect(() => {
-        if(libraryName === "regular") {
-            setIconLibrary(RegularIcons);
-        } else if(libraryName === "spiegel") {
-            setIconLibrary(SpiegelIcons);
+        if(libraryName) {
+            if(libraryName === "regular") {
+                import('@logora/debate.icons.regular_icons').then(library => {
+                    setIconLibrary(library);
+                });
+            } else if(libraryName === "spiegel") {
+                import('@logora/debate.icons.spiegel_icons').then(library => {
+                    setIconLibrary(library);
+                });
+            }
         }
     }, [libraryName]);
 
