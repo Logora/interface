b6e9fc96f5aa6b67224deb7ef41ff74c17768159
Henry Boisgibault <h.boisgibault@gmail.com>
2023-10-21 12:02:23 +0200

    Refactor IconProvider to remove async loading

--- src/components/icons/icon_provider/IconProvider.jsx
+++ src/components/icons/icon_provider/IconProvider.jsx
@@ -2,19 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { IconContext } from './IconContext';
 import PropTypes from "prop-types";
 
-export const IconProvider = ({ library = "regular", async = false, children }) => {
+export const IconProvider = ({ library, children }) => {
     const [iconLibrary, setIconLibrary] = useState(null);
 
     useEffect(() => {
-        if(library) {
-            if(async) {
-                import(`@logora/debate.icons.${library}_icons`).then(library => {
-                    setIconLibrary(library);
-                });
-            } else {
-                setIconLibrary(library);
-            }
-        }
+        setIconLibrary(library);
     }, [library]);
 
     return (
@@ -26,14 +18,9 @@ export const IconProvider = ({ library = "regular", async = false, children }) =
 
 IconProvider.propTypes = {
     /** Library name if async or module */
-    library: PropTypes.string,
+    library: PropTypes.string.isRequired,
     /** Whether to load the library asynchronously or not */
     async: PropTypes.bool,
     /** Provider children */
 	children: PropTypes.node
 };
-
-IconProvider.defaultProps = {
-    library: "regular",
-    async: false
-}
