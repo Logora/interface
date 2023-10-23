6f4a147c9f1b243dab4aa29d48d324cd79dd2f35
Neko-cat <mathilde.couvreur@gmail.com>
2023-10-23 12:01:14 +0200

    Modify date and tests for UserBox

--- src/components/user/user_box/UserBox.composition.js
+++ src/components/user/user_box/UserBox.composition.js
@@ -6,7 +6,7 @@ import { Location } from '@logora/debate.util.location';
 import { BrowserRouter } from 'react-router-dom';
 import { faker } from '@faker-js/faker';
 
-export const user = {
+const user = {
     id: 82,
     uid: "296deae0-15e3-46dd-bd85-498456809453",
     hash_id: "296deae0-15e3-46dd",
@@ -16,7 +16,7 @@ export const user = {
     score: 0,
     points: 1539,
     description: null,
-    last_activity: "2023-02-10T12:22:18.192Z",
+    last_activity: faker.date.recent(),
     groups_count: 5,
     upvotes: 13,
     messages_count: 20,
