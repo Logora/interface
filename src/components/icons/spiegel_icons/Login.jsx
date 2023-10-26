d9ebbf7c576120b80118fbc615c2d5cb683f9481
Mboulgour <mboulgour@protonmail.com>
2023-10-26 11:29:33 +0200

    💄 Update filling for Login icon

--- src/components/icons/spiegel_icons/Login.jsx
+++ src/components/icons/spiegel_icons/Login.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export const Login = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-        <path d="M7,22H5a3,3,0,0,1-3-3V5A3,3,0,0,1,5,2H7A1,1,0,0,0,7,0H5A5.006,5.006,0,0,0,0,5V19a5.006,5.006,0,0,0,5,5H7a1,1,0,0,0,0-2Z"/>
-        <path d="M18.538,18.707l4.587-4.586a3.007,3.007,0,0,0,0-4.242L18.538,5.293a1,1,0,0,0-1.414,1.414L21.416,11H6a1,1,0,0,0,0,2H21.417l-4.293,4.293a1,1,0,1,0,1.414,1.414Z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" fill="none" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
     </svg>
 )
