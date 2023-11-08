1491e670052cd642d2c91955082fbe489b0f708f
Henry Boisgibault <h.boisgibault@gmail.com>
2023-11-08 19:30:01 +0100

    Add IconContext to IconProvider

--- src/components/icons/icon_provider/IconProvider.jsx
+++ src/components/icons/icon_provider/IconProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect , createContext} from 'react';
 import PropTypes from "prop-types";
 
 export const IconContext = createContext();
