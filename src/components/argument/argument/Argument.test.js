c7b1e2e16fa4c962b4a617666479af7864da3fda
Neko-cat <mathilde.couvreur@gmail.com>
2024-02-22 18:09:08 +0100

    Update Argument tests

--- src/components/argument/argument/Argument.test.js
+++ src/components/argument/argument/Argument.test.js
@@ -76,6 +76,99 @@ const argument = {
     }
 };
 
+const argumentWithReplies = {
+    id: 414,
+    content: faker.lorem.sentences(2),
+    upvotes: 0,
+    group_id: 417,
+    is_reply: false,
+    created_at: faker.date.recent(),
+    is_deleted: false,
+    score: 50,
+    author: {
+        image_url: faker.image.avatar(),
+        full_name: faker.name.fullName(),
+        hash_id: faker.lorem.slug(),
+        slug: faker.lorem.slug(),
+        points: 1320,
+        last_activity: new Date(),
+        description: faker.name.jobTitle()
+    },
+    position: {
+        id: 1,
+        name: "Yes",
+        language: "en",
+        translation_entries: []
+    },
+    number_replies: 3,
+    replies_authors: [
+        {
+            image_url: faker.image.avatar(),
+            full_name: faker.name.fullName(),
+        },
+        {
+            image_url: faker.image.avatar(),
+            full_name: faker.name.fullName(),
+        },
+        {
+            image_url: faker.image.avatar(),
+            full_name: faker.name.fullName(),
+        },
+    ]
+};
+
+const argumentReply = {
+    id: 415,
+    content: faker.lorem.sentences(2),
+    upvotes: 12,
+    group_id: 417,
+    is_reply: true,
+    created_at: faker.date.recent(),
+    is_deleted: false,
+    score: 50,
+    author: {
+        image_url: faker.image.avatar(),
+        full_name: faker.name.fullName(),
+        hash_id: faker.lorem.slug(),
+        slug: faker.lorem.slug(),
+        points: 1320,
+        last_activity: new Date(),
+        description: faker.name.jobTitle()
+    },
+    position: {
+        id: 2,
+        name: "No",
+        language: "en",
+        translation_entries: []
+    }
+};
+
+const argumentDeleted = {
+    id: 416,
+    content: faker.lorem.sentences(1),
+    upvotes: 12,
+    group_id: 417,
+    is_reply: false,
+    created_at: faker.date.recent(),
+    is_deleted: true,
+    score: 50,
+    author: {
+        image_url: faker.image.avatar(),
+        full_name: faker.name.fullName(),
+        hash_id: faker.lorem.slug(),
+        slug: faker.lorem.slug(),
+        points: 1320,
+        last_activity: new Date(),
+        description: faker.name.jobTitle()
+    },
+    position: {
+        id: 2,
+        name: "No",
+        language: "en",
+        translation_entries: []
+    }
+};
+
 const debatePositions = [
     {
         id: 1,
@@ -94,7 +187,7 @@ const debatePositions = [
 const debateName = faker.lorem.sentence(5);
 
 describe('Argument', () => {
-    it ('should render correctly', () => {  
+    it ('should render argument correctly', () => {  
         const { getByText } = render(
             <BrowserRouter>
                 <ConfigProvider routes={{ ...routes }} config={{ translation: { enable: false } }}>
@@ -113,7 +206,6 @@ describe('Argument', () => {
                                                                     argument={argument}
                                                                     debatePositions={debatePositions}
                                                                     debateName={debateName}
-                                                                    replies={false}
                                                                     nestingLevel={0}
                                                                     debateIsActive
                                                                 />
@@ -131,9 +223,144 @@ describe('Argument', () => {
                 </ConfigProvider>
             </BrowserRouter>
         );
+
         expect(getByText(argument.author.full_name)).toBeInTheDocument();
         expect(getByText(argument.position.name)).toBeInTheDocument();
         expect(getByText("Reply")).toBeInTheDocument();
         expect(getByText("Share")).toBeInTheDocument();
+    });
+
+    it ('should render argument with replies', () => {  
+        const { getByText, getByAltText } = render(
+            <BrowserRouter>
+                <ConfigProvider routes={{ ...routes }} config={{ translation: { enable: false } }}>
+                    <DataProviderContext.Provider value={{ dataProvider: data }}>
+                        <AuthContext.Provider value={{ currentUser: currentUser, isLoggedIn: true }}>
+                            <ResponsiveProvider>
+                                <ModalProvider>
+                                    <ListProvider>
+                                        <ToastProvider>
+                                            <VoteProvider>
+                                                <IdProvider>
+                                                    <InputProvider>
+                                                        <IconProvider library={regularIcons}>
+                                                            <IntlProvider locale="en">
+                                                                <Argument
+                                                                    argument={argumentWithReplies}
+                                                                    debatePositions={debatePositions}
+                                                                    debateName={debateName}
+                                                                    nestingLevel={0}
+                                                                    debateIsActive
+                                                                />
+                                                            </IntlProvider>
+                                                        </IconProvider>
+                                                    </InputProvider>
+                                                </IdProvider>
+                                            </VoteProvider>
+                                        </ToastProvider>
+                                    </ListProvider>
+                                </ModalProvider>
+                            </ResponsiveProvider>
+                        </AuthContext.Provider>
+                    </DataProviderContext.Provider>
+                </ConfigProvider>
+            </BrowserRouter>
+        );
+        
+        expect(getByText("View answers")).toBeInTheDocument();
+
+        const user1Img = getByAltText(`${argumentWithReplies.replies_authors[0].full_name}'s profile picture`);
+        const user2Img = getByAltText(`${argumentWithReplies.replies_authors[1].full_name}'s profile picture`);
+        const user3Img = getByAltText(`${argumentWithReplies.replies_authors[2].full_name}'s profile picture`);
+        expect(user1Img).toBeInTheDocument();
+        expect(user2Img).toBeInTheDocument();
+        expect(user3Img).toBeInTheDocument();
+        expect(user1Img).toHaveAttribute('src', argumentWithReplies.replies_authors[0].image_url);
+        expect(user2Img).toHaveAttribute('src', argumentWithReplies.replies_authors[1].image_url);
+        expect(user3Img).toHaveAttribute('src', argumentWithReplies.replies_authors[2].image_url);
+    });
+
+    it ('should render argument as a reply', () => {  
+        const { container } = render(
+            <BrowserRouter>
+                <ConfigProvider routes={{ ...routes }} config={{ translation: { enable: false } }}>
+                    <DataProviderContext.Provider value={{ dataProvider: data }}>
+                        <AuthContext.Provider value={{ currentUser: currentUser, isLoggedIn: true }}>
+                            <ResponsiveProvider>
+                                <ModalProvider>
+                                    <ListProvider>
+                                        <ToastProvider>
+                                            <VoteProvider>
+                                                <IdProvider>
+                                                    <InputProvider>
+                                                        <IconProvider library={regularIcons}>
+                                                            <IntlProvider locale="en">
+                                                                <Argument
+                                                                    argument={argumentReply}
+                                                                    debatePositions={debatePositions}
+                                                                    debateName={debateName}
+                                                                    nestingLevel={0}
+                                                                    debateIsActive
+                                                                />
+                                                            </IntlProvider>
+                                                        </IconProvider>
+                                                    </InputProvider>
+                                                </IdProvider>
+                                            </VoteProvider>
+                                        </ToastProvider>
+                                    </ListProvider>
+                                </ModalProvider>
+                            </ResponsiveProvider>
+                        </AuthContext.Provider>
+                    </DataProviderContext.Provider>
+                </ConfigProvider>
+            </BrowserRouter>
+        );
+
+        const argumentContainer = container.querySelector(`#argument_${argumentReply.id}`);
+        expect(argumentContainer).toHaveClass('argumentReply');
+    });
+
+    it ('should render comment argument', () => {  
+        const { queryByText, getByTestId } = render(
+            <BrowserRouter>
+                <ConfigProvider routes={{ ...routes }} config={{ translation: { enable: false } }}>
+                    <DataProviderContext.Provider value={{ dataProvider: data }}>
+                        <AuthContext.Provider value={{ currentUser: currentUser, isLoggedIn: true }}>
+                            <ResponsiveProvider>
+                                <ModalProvider>
+                                    <ListProvider>
+                                        <ToastProvider>
+                                            <VoteProvider>
+                                                <IdProvider>
+                                                    <InputProvider>
+                                                        <IconProvider library={regularIcons}>
+                                                            <IntlProvider locale="en">
+                                                                <Argument
+                                                                    argument={argument}
+                                                                    debatePositions={debatePositions}
+                                                                    debateName={debateName}
+                                                                    nestingLevel={0}
+                                                                    debateIsActive
+                                                                    isComment
+                                                                />
+                                                            </IntlProvider>
+                                                        </IconProvider>
+                                                    </InputProvider>
+                                                </IdProvider>
+                                            </VoteProvider>
+                                        </ToastProvider>
+                                    </ListProvider>
+                                </ModalProvider>
+                            </ResponsiveProvider>
+                        </AuthContext.Provider>
+                    </DataProviderContext.Provider>
+                </ConfigProvider>
+            </BrowserRouter>
+        );
+
+        expect(queryByText("Share")).not.toBeInTheDocument();
+        const replyButton = getByTestId('reply-button');
+        expect(replyButton).toHaveClass('leftReply');
     });
 })
