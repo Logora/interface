2bebacc99c1565c06db3d8937e0c2ec12ccf6ba3
Henry Boisgibault <h.boisgibault@gmail.com>
2024-02-23 15:49:04 +0100

    Update TranslatedContent usage in DebateBox component

--- src/components/debate/debate_box/DebateBox.jsx
+++ src/components/debate/debate_box/DebateBox.jsx
@@ -6,7 +6,7 @@ import { useIntl, FormattedMessage } from 'react-intl';
 import { Icon } from '@logora/debate.icons.icon';
 import { getWinningVote } from '@logora/debate.util.get_winning_vote';
 import { useResponsive } from '@logora/debate.hooks.use_responsive';
-import { useTranslatedContent } from '@logora/debate.translation.translated_content';
+import { TranslatedContent, useTranslatedContent } from '@logora/debate.translation.translated_content';
 import cx from 'classnames';
 import styles from './DebateBox.module.scss';
 import PropTypes from "prop-types";
@@ -17,14 +17,13 @@ export const DebateBox = ({ debate }) => {
     const config = useConfig();
     const routes = useRoutes();
     const intl = useIntl();
-    const name = useTranslatedContent(debate.name, debate.language, "name", debate.translation_entries);
     const { isMobile } = useResponsive();
-    const winningPositionName = useTranslatedContent(winningPosition.name, winningPosition.language, "name", winningPosition.translation_entries);
+    const name = useTranslatedContent(debate.name, debate.language, "name", debate.translation_entries);
 
     useEffect(() => {
         setTotalVotes(getWinningVote(debate.votes_count, debate.group_context.positions).totalVotes);
-		setWinningPosition(getWinningVote(debate.votes_count, debate.group_context.positions).winningPositionObj);
-	}, [])
+        setWinningPosition(getWinningVote(debate.votes_count, debate.group_context.positions).winningPositionObj);
+    }, [])
 
     const getPercentageValue = (voteCount, totalVotes) => {
         if (totalVotes == 0 || voteCount == 0) { return 0; }
@@ -44,38 +43,37 @@ export const DebateBox = ({ debate }) => {
     return (
         <div className={styles.debateBox}>
             <div className={styles.debateBoxHeader}>
-            <div className={styles.debateBoxImageBox}>
-                <Link to={routes.debateShowLocation.toUrl({debateSlug: debate.slug})} className={styles.imageLink}>
-                    <img data-tid={"view_debate_image"} loading={"lazy"} className={styles.debateBoxImage} src={debate.image_url} height={200} alt={intl.formatMessage({ id:"debate.debate_box.alt_debate_image", defaultMessage: "Debate image " }) } />
-                </Link>
-                { debate.is_active === false &&
-                    <div className={styles.inactiveDebate}>
-                        <FormattedMessage id="debate.debate_box.debate_is_inactive" defaultMessage={"Debate is ended"} />
+                <div className={styles.debateBoxImageBox}>
+                    <Link to={routes.debateShowLocation.toUrl({ debateSlug: debate.slug })} className={styles.imageLink}>
+                        <img data-tid={"view_debate_image"} loading={"lazy"} className={styles.debateBoxImage} src={debate.image_url} height={200} alt={intl.formatMessage({ id: "debate.debate_box.alt_debate_image", defaultMessage: "Debate image " })} />
+                    </Link>
+                    {debate.is_active === false &&
+                        <div className={styles.inactiveDebate}>
+                            <FormattedMessage id="debate.debate_box.debate_is_inactive" defaultMessage={"Debate is ended"} />
+                        </div>
+                    }
+                </div>
+                {debate.group_context.author && debate.group_context.author.is_admin === false &&
+                    <div className={styles.debateSuggestion}>
+                        <Icon name="suggestion" width={15} height={15} />
+                        <span className={styles.authorSuggestion}>
+                            <FormattedMessage id="debate.debate_box.suggestion_author_short" defaultMessage={"Suggested by"} />
+                            <span className={styles.authorName}>{debate.group_context.author.full_name}</span>
+                        </span>
                     </div>
                 }
             </div>
-            { debate.group_context.author && debate.group_context.author.is_admin === false &&
-                <div className={styles.debateSuggestion}>
-                    <Icon name="suggestion" width={15} height={15} />
-                    <span className={styles.authorSuggestion}>
-                        <FormattedMessage id="debate.debate_box.suggestion_author_short" defaultMessage={"Suggested by"} />
-                        <span className={styles.authorName}>{debate.group_context.author.full_name}</span>
-                    </span>
-                </div>
-            }
-            
-            </div>
             <div className={styles.debateBoxBody}>
-                <div className={styles.debateBoxTitle} title={ name.translatedContent }>
-                    <Link to={routes.debateShowLocation.toUrl({debateSlug: debate.slug})}>
+                <div className={styles.debateBoxTitle} title={name.translatedContent}>
+                    <Link to={routes.debateShowLocation.toUrl({ debateSlug: debate.slug })}>
                         <>
-                            { isMobile ?
+                            {isMobile ?
                                 <div data-tid={"view_debate_title"} className={cx(styles.debateTitle, styles.debateTitleMobile)}>
-                                    { name.translatedContent }
+                                    {name.translatedContent}
                                 </div>
-                            :
+                                :
                                 <div data-tid={"view_debate_title"} className={styles.debateTitle}>
-                                    { name.translatedContent }
+                                    {name.translatedContent}
                                 </div>
                             }
                         </>
@@ -83,14 +81,14 @@ export const DebateBox = ({ debate }) => {
                 </div>
                 <div className={styles.debateBoxFooter}>
                     <div className={styles.debateParticipantsBox}>
-                        { debate.participants.length === 0 ? (
-                            <span className={styles.debateParticipantsEmpty}>{intl.formatMessage({ id:"debate.debate_box.fallback_no_participants", defaultMessage:"No participants" }) }</span>
+                        {debate.participants.length === 0 ? (
+                            <span className={styles.debateParticipantsEmpty}>{intl.formatMessage({ id: "debate.debate_box.fallback_no_participants", defaultMessage: "No participants" })}</span>
                         ) : (
                             <>
                                 {debate.participants.map(displayParticipant)}
-                                {debate.participants_count > 3 && 
+                                {debate.participants_count > 3 &&
                                     <div className={styles.debateParticipantItem}>
-                                        <Link to={routes.debateShowLocation.toUrl({debateSlug: debate.slug})}>
+                                        <Link to={routes.debateShowLocation.toUrl({ debateSlug: debate.slug })}>
                                             <div className={styles.participantsCountBox}>
                                                 +{intl.formatNumber(debate.participants_count - 3, { notation: 'compact', maximumFractionDigits: 1, roundingMode: "floor" })}
                                             </div>
@@ -105,10 +103,15 @@ export const DebateBox = ({ debate }) => {
                         :
                         <div className={styles.debateBoxNumbers}>
                             <div className={styles.debateBoxNumbersText}>
-                                { winningPosition && (
+                                {winningPosition && (
                                     <>
-                                        { getPercentageValue(winningPosition.count, totalVotes) } %{" "}
-                                        { winningPositionName.translatedContent || winningPosition.name }
+                                        {getPercentageValue(winningPosition.count, totalVotes)} %{" "}
+                                        <TranslatedContent
+                                            originalContent={winningPosition.name}
+                                            originalLanguage={winningPosition.language}
+                                            targetField={"name"}
+                                            translations={winningPosition.translation_entries}
+                                        />
                                     </>
                                 )}
                             </div>
