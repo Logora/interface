255923a3b01b965ba60d233125b9afde256aff92
Henry Boisgibault <h.boisgibault@gmail.com>
2024-04-12 14:42:42 +0200

    Remove unnecessary test code in Drawer.test.js

--- src/components/dialog/drawer/Drawer.test.js
+++ src/components/dialog/drawer/Drawer.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { DefaultDrawer } from './Drawer.composition';
-import { render, screen } from '@testing-library/react';
 
 describe('Drawer', () => {
     it('should open drawer when clicking on button', async () => {
@@ -9,8 +8,6 @@ describe('Drawer', () => {
         );
 
         const openButton = screen.getByTestId("open-button");
-        const closeButton = screen.getByTestId("close-button");
-        const closeButtonStyles = getComputedStyle(closeButton);
 
         expect(openButton).toBeTruthy();
     });
