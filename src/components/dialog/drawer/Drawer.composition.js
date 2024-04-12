738afb947463e24fc88c540df466df5f008a4ac3
Henry Boisgibault <h.boisgibault@gmail.com>
2024-04-12 13:34:27 +0200

    Add react-modern-drawer component to dialog/drawer

---
+++ src/components/dialog/drawer/Drawer.composition.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { Drawer } from './Drawer';
+import { IconProvider } from '@logora/debate.icons.icon_provider';
+import * as regularIcons from '@logora/debate.icons.regular_icons';
+
+export const DefaultDrawer = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState);
+    };
+
+    return (
+        <IconProvider library={regularIcons}>
+            <div onClick={toggleDrawer}>Click here to toggle drawer</div>
+
+            <Drawer isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                <div>Drawer content</div>
+            </Drawer>
+        </IconProvider>
+    )
+};
+
+export const DrawerWithOverlay = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState);
+    };
+
+    return (
+        <IconProvider library={regularIcons}>
+            <div onClick={toggleDrawer}>Click here to toggle drawer</div>
+
+            <Drawer isOpen={isOpen} onClose={() => setIsOpen(false)} enableOverlay>
+                <div>Drawer content</div>
+            </Drawer>
+        </IconProvider>
+    )
+};
+
+export const DrawerWithTitle = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState);
+    };
+
+    return (
+        <IconProvider library={regularIcons}>
+            <div onClick={toggleDrawer}>Click here to toggle drawer</div>
+
+            <Drawer isOpen={isOpen} title={"My drawer"} onClose={() => setIsOpen(false)}>
+                <div>Drawer content</div>
+            </Drawer>
+        </IconProvider>
+    )
+};
+
+export const SmallDrawer = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState);
+    };
+
+    return (
+        <IconProvider library={regularIcons}>
+            <div onClick={toggleDrawer}>Click here to toggle drawer</div>
+
+            <Drawer isOpen={isOpen} onClose={() => setIsOpen(false)} size={400}>
+                <div>Drawer content</div>
+            </Drawer>
+        </IconProvider>
+    )
+};
