db6280da099ffbaf6132e47814bbb5563f0d8f20
Henry Boisgibault <h.boisgibault@gmail.com>
2024-04-12 14:52:39 +0200

    Add testing for opening drawer on button click

--- src/components/dialog/drawer/Drawer.test.js
+++ src/components/dialog/drawer/Drawer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DefaultDrawer } from './Drawer.composition';
+import { render, screen } from '@testing-library/react';
 
 describe('Drawer', () => {
     it('should open drawer when clicking on button', async () => {
