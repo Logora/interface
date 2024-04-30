b07c83584636a625717b0d3f91c51d46750aec73
Mboulgour <mboulgour@protonmail.com>
2024-04-30 16:48:11 +0200

    style: Improve tag component layout and add close icon to active tags

--- src/components/tag/tag/Tag.composition.js
+++ src/components/tag/tag/Tag.composition.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Tag } from './Tag';
+import { IconProvider } from '@logora/debate.icons.icon_provider';
+import * as regularIcons from '@logora/debate.icons.regular_icons';
 
 export const ActiveTag = () => {
-    return <Tag active text="Active" />;
+    return (
+        <IconProvider library={regularIcons}>
+            <Tag active text="Active" />
+        </IconProvider>
+    )
 };
 
 export const InactiveTag = () => {
-    return <Tag active={false} text="Inactive" />;
+    return (
+        <IconProvider library={regularIcons}>
+            <Tag active={false} text="Inactive" />
+        </IconProvider>
+    )
 };
--- src/components/tag/tag/Tag.jsx
+++ src/components/tag/tag/Tag.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import cx from 'classnames';
 import styles from './Tag.module.scss';
 import PropTypes from "prop-types";
+import { Icon } from "@logora/debate.icons.icon";
 
 export const Tag = ({ dataTid, active, text, className }) => {
     return (
-        <span data-testid="tag" data-tid={dataTid ? dataTid : null} className={cx(styles.tag, styles.className, className, { [styles.active]: active })}>{ text }</span>
+        <span data-testid="tag" data-tid={dataTid ? dataTid : null} className={cx(styles.tag, styles.className, className, { [styles.active]: active })}>
+			{ text } 
+			{ active && <Icon name="close" height={10} width={10} /> }
+		</span>
     )
 }
 
