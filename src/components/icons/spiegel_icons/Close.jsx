f6b1abaae7bbffe48f76a6dfe81b6fe818dacebe
Henry Boisgibault <h.boisgibault@gmail.com>
2024-06-06 14:25:56 +0200

    Update Close icon stroke color to black

--- src/components/icons/spiegel_icons/Close.jsx
+++ src/components/icons/spiegel_icons/Close.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Close = (props) => (
-    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg viewBox="0 0 16 16" fill="none" stroke="black" xmlns="http://www.w3.org/2000/svg" {...props}>
         <path d="M12 12L4.00002 4M12 4L4.00002 12" stroke-linecap="round"/>
     </svg>
 )
