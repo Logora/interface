be47eb238bdb494b11e7572514ed521fefb92681
somayaassaadi <somaya.assaadi@gmail.com>
2024-07-15 17:34:24 +0200

    Added notification menu

---
+++ src/components/notification/notification_menu/NotificationMenu.composition.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { NotificationMenu } from './NotificationMenu';
+import { IntlProvider } from 'react-intl';
+import { IconProvider } from '@logora/debate.icons.icon_provider';
+import { ConfigProvider } from '@logora/debate.data.config_provider';
+import { dataProvider, DataProviderContext } from '@logora/debate.data.data_provider';
+import { MemoryRouter } from "react-router";
+import * as regularIcons from '@logora/debate.icons.regular_icons';
+import { faker } from '@faker-js/faker';
+
+const notificationDefinitions = {
+  new_comment: {
+    getRedirectUrl: () => '/comments/1',
+    getImage: () => <img src={faker.image.abstract()} />,
+    getContent: () => faker.commerce.productDescription()
+  },
+}
+
+const httpClient = {
+  post: () => {
+    return new Promise(function (resolve) {
+      resolve({ data: { success: true, data: {} } });
+    });
+  }
+};
+
+const data = dataProvider(httpClient, "https://mock.example.api");
+
+const NotificationComponent = () => {
+  return (
+    <div>
+      {}
+    </div>
+  );
+};
+
+export const DefaultNotificationMenu = () => {
+  return (
+    <MemoryRouter>
+      <ConfigProvider config={{}}>
+        <DataProviderContext.Provider value={{ dataProvider: data }}>
+          <IntlProvider locale="en">
+            <IconProvider library={regularIcons}>
+              <NotificationMenu
+                Notification={NotificationComponent}
+                NotificationDefinitions={notificationDefinitions}
+              />
+            </IconProvider>
+          </IntlProvider>
+        </DataProviderContext.Provider>
+      </ConfigProvider>
+    </MemoryRouter>
+  );
+};
+
+export const NotificationMenuWithReadAll = () => {
+  return (
+    <MemoryRouter>
+      <ConfigProvider config={{}}>
+        <DataProviderContext.Provider value={{ dataProvider: data }}>
+          <IntlProvider locale="en">
+            <IconProvider library={regularIcons}>
+              <NotificationMenu
+                Notification={NotificationComponent}
+                NotificationDefinitions={notificationDefinitions}
+                initialReadAll={true}
+              />
+            </IconProvider>
+          </IntlProvider>
+        </DataProviderContext.Provider>
+      </ConfigProvider>
+    </MemoryRouter>
+  );
+};
---
+++ src/components/notification/notification_menu/NotificationMenu.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useDataProvider } from '@logora/debate.data.data_provider';
+import { useIntl } from "react-intl";
+import { PaginatedList } from "@logora/debate.list.paginated_list";
+import { UserContentSkeleton } from '@logora/debate.skeleton.user_content_skeleton';
+import styles from "./NotificationMenu.module.scss";
+import PropTypes from "prop-types";
+
+
+export const NotificationMenu = ({
+  Notification,
+  NotificationDefinitions,
+}) => {
+  const intl = useIntl();
+  const api = useDataProvider();
+  const [readAll, setReadAll] = useState(false);
+
+  const handleClick = () => {
+    setReadAll(true);
+    api.create("notifications/read/all", {}).then((response) => {});
+  };
+
+  const handleKeyDown = (event, method) => {
+    const ENTER_KEY = 13;
+    if (event.keyCode == ENTER_KEY) {
+      if (method === "readAllNotifications") {
+        handleClick();
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.notificationMenuHeader}>
+        <div className={styles.notificationMenuHeaderText}>
+          { intl.formatMessage({ id: 'header.notifications' }) }
+        </div>
+        <div
+          id='read_all_notifications'
+          data-tid={"action_read_all_notifications"}
+          className={styles.readNotificationsButton}
+          tabIndex='0'
+          onKeyDown={(event) => handleKeyDown(event, "readAllNotifications")}
+          onClick={handleClick}
+        >
+          { intl.formatMessage({ id: 'notifications.read_all' }) }
+        </div>
+      </div>
+      <div className={styles.notificationList}>
+        <PaginatedList 
+          currentListId={"notificationList"}
+          loadingComponent={<UserContentSkeleton numberLines={0} />}
+          resourcePropName="notification"
+          resource={'notifications'} 
+          sort={"-created_at"}
+          perPage={10}
+          withToken={true}
+          display={"column"}
+          gap={"0px"}
+        >
+          <Notification 
+            notificationDefinitions={NotificationDefinitions}
+            isOpen={readAll} 
+          />
+        </PaginatedList>
+      </div>
+    </>
+  );
+}
+
+NotificationMenu.propTypes = {
+  Notification: PropTypes.elementType.isRequired,
+  NotificationDefinitions: PropTypes.object.isRequired,
+  initialReadAll: PropTypes.bool,
+};
+
+NotificationMenu.defaultProps = {
+  initialReadAll: false,
+};
+
