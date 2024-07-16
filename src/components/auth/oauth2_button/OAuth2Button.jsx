1bc51d5166e501fb00159ea1e3a768a7c70992fa
Paul-Henry Ngounou <102412853+Paul-HenryN@users.noreply.github.com>
2024-07-16 15:56:36 +0200

    Feat/oauth2 button link mode (#71)
    * feat: add link popup prop to OAuth2Button component
    
    * fix(oauth2 button): remove className from a element in link mode
    
    * test(oauth2 button): add link mode tests

--- src/components/auth/oauth2_button/OAuth2Button.jsx
+++ src/components/auth/oauth2_button/OAuth2Button.jsx
@@ -1,71 +1,98 @@
 import React from 'react';
-import { useLocation } from "react-router";
-import OauthPopup from "react-oauth-popup";
+import { useLocation } from 'react-router';
+import OauthPopup from 'react-oauth-popup';
 import styles from './OAuth2Button.module.scss';
 import classnames from 'classnames';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-export const OAuth2Button = ({ provider, authDialogUrl, clientId, redirectUri, scope, responseType, onCode, onClose, children, className }) =>  {
-    const location = useLocation();
+export const OAuth2Button = ({
+  provider,
+  authDialogUrl,
+  clientId,
+  redirectUri,
+  scope,
+  responseType,
+  onCode,
+  onClose,
+  popup = true,
+  children,
+  className,
+}) => {
+  const location = useLocation();
 
-	const handleOnCode = (code, params) => {
-		onCode(code, provider);
-	}
+  const handleOnCode = (code, params) => {
+    onCode(code, provider);
+  };
 
-	const handleOnClose = () => {
-        if(onClose) {
-            onClose();
-        }
-	}
+  const handleOnClose = () => {
+    if (onClose) {
+      onClose();
+    }
+  };
 
-	const getDialogUrl = () => {
-		let baseUrl = new URL(authDialogUrl);
-		baseUrl.searchParams.append("client_id", clientId);
-		baseUrl.searchParams.append("redirect_uri", redirectUri);
-		baseUrl.searchParams.append("scope", scope);
-		if(responseType) {
-			baseUrl.searchParams.append("response_type", responseType);
-		}
-		if(typeof window !== 'undefined') {
-			baseUrl.searchParams.append("state", window.btoa(window.location.origin + location.pathname + location.hash + location.search));
-		}
-		return baseUrl.href;
-	}
+  const getDialogUrl = () => {
+    let baseUrl = new URL(authDialogUrl);
+    baseUrl.searchParams.append('client_id', clientId);
+    baseUrl.searchParams.append('redirect_uri', redirectUri);
+    baseUrl.searchParams.append('scope', scope);
+    if (responseType) {
+      baseUrl.searchParams.append('response_type', responseType);
+    }
+    if (typeof window !== 'undefined') {
+      baseUrl.searchParams.append(
+        'state',
+        window.btoa(
+          window.location.origin +
+            location.pathname +
+            location.hash +
+            location.search
+        )
+      );
+    }
+    return baseUrl.href;
+  };
 
-	return (
-		<div data-testid="container" className={classnames(styles.oauthButtonContainer, className)}>
-			<OauthPopup
-				url={getDialogUrl()}
-				onCode={handleOnCode}
-				onClose={handleOnClose}
-			>
-                <div className={styles.oauthPopupContainer}>
-                    { children }
-                </div>
-			</OauthPopup>
-		</div>
-	);
-}
+  return (
+    <div
+      data-testid="container"
+      className={classnames(styles.oauthButtonContainer, className)}
+    >
+      {popup ? (
+        <OauthPopup
+          url={getDialogUrl()}
+          onCode={handleOnCode}
+          onClose={handleOnClose}
+        >
+          <div className={styles.oauthPopupContainer}>{children}</div>
+        </OauthPopup>
+      ) : (
+        <a href={getDialogUrl()}>{children}</a>
+      )}
+    </div>
+  );
+};
 
 OAuth2Button.propTypes = {
-	/** Provider name that will be passed with the onCode callback */
-	provider: PropTypes.string,
-	/** OAuth login dialog URL */
-	authDialogUrl: PropTypes.string,
-	/** OAuth2 client ID */
-	clientId: PropTypes.string,
-	/** OAuth2 redirect URI */
-	redirectUri: PropTypes.string,
-	/** OAuth2 scope */
-	scope: PropTypes.string,
-	/** OAuth2 response type */
-	responseType: PropTypes.string,
-	/** Callback triggered if auth is sucessful */
-	onCode: PropTypes.func,
-	/** Callback triggered if popup is closed */
-	onClose: PropTypes.func,
-	/** Button content */
-	children: PropTypes.node,
-	/**  Class name to style the button container */
-	className: PropTypes.string,
-}
+  /** Provider name that will be passed with the onCode callback */
+  provider: PropTypes.string,
+  /** OAuth login dialog URL */
+  authDialogUrl: PropTypes.string,
+  /** OAuth2 client ID */
+  clientId: PropTypes.string,
+  /** OAuth2 redirect URI */
+  redirectUri: PropTypes.string,
+  /** OAuth2 scope */
+  scope: PropTypes.string,
+  /** OAuth2 response type */
+  responseType: PropTypes.string,
+  /** Callback triggered if auth is sucessful */
+  onCode: PropTypes.func,
+  /** Callback triggered if popup is closed */
+  onClose: PropTypes.func,
+  /** Button content */
+  children: PropTypes.node,
+  /**  Class name to style the button container */
+  className: PropTypes.string,
+  /**  Open a popup on click. If false, the button will be a link */
+  popup: PropTypes.bool,
+};
