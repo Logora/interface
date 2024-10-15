bd8270ed720e123173df5bbc895251d7c33a1439
Mboulgour <mboulgour@protonmail.com>
2024-10-15 16:26:05 +0200

    style: Improve conditional logic in ArgumentInput component

--- src/components/input/argument_input/ArgumentInput.jsx
+++ src/components/input/argument_input/ArgumentInput.jsx
@@ -305,8 +305,8 @@ export const ArgumentInput = ({ argumentListId, avatarSize = 48, disabled = fals
                             </div>
                         }
                         <div className={cx(styles.argumentTextInputBox, {[styles.argumentTextInputBoxisTablet]: isDesktop, [styles.replyEditorRow]: isReply})}>
-                            <div className={cx(styles.argumentAuthorContainer,{[styles.argumentAuthorContainerMobile]: isMobile, [styles.argumentAuthorContainerActivated]: inputActivation || isReply})}>
-                                { inputActivation || isReply ?
+                            <div className={cx(styles.argumentAuthorContainer,{[styles.argumentAuthorContainerMobile]: isMobile, [styles.argumentAuthorContainerActivated]: (!isMobile && inputActivation) || isReply})}>
+                                { (!isMobile && inputActivation) || isReply ?
                                     <Avatar avatarUrl={currentUser.image_url} userName={currentUser.full_name} size={avatarSize} />
                                 :
                                     <AuthorBox
