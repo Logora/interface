01bb12c544089605d16e884989294e245ee99397
somayaassaadi <103968938+somayaassaadi@users.noreply.github.com>
2024-10-15 10:40:53 +0200

    [SuggestionBanner] Added suggestion banner component (#118)
    * [SuggestionBanner] Added suggestion banner component
    
    * [SuggestionBanner]Updated suggestionbanner composition
    
    * [SuugestionBanner] Added composition, doc and test to suggestionBanner
    
    * [SuggestionBanner] Added emptysuggestionabanner to SuggestionBannerComposition
    
    * [SuggetsionBanner] Changed SugestionsBanner to SuggestionBanner (removed the 's') and updated the doc
    
    * [SuggestionBanner] Format Document
    
    * [SuggestionBanner] Added a test when there are no suggestions
    
    * [SuggestionBanner] Format Document test
    
    * Update SuggestionBanner configuration and styling
    
    ---------
    
    Co-authored-by: Henry Boisgibault <h.boisgibault@gmail.com>

---
+++ src/components/suggestion/suggestion_banner/SuggestionBanner.jsx
@@ -0,0 +1,66 @@
+import React, { useMemo } from "react";
+import { useResponsive } from "@logora/debate.hooks.use_responsive";
+import { useRoutes } from '@logora/debate.data.config_provider';
+import { useIntl } from 'react-intl';
+import { Button } from '@logora/debate.action.button';
+import { VotePaginatedList } from "@logora/debate.list.paginated_list";
+import { Icon } from "@logora/debate.icons.icon";
+import { FormattedMessage } from "react-intl";
+import { UserContentSkeleton } from '@logora/debate.skeleton.user_content_skeleton';
+import { SuggestionBox } from '@logora/debate.suggestion.suggestion_box';
+import cx from 'classnames';
+import styles from "./SuggestionBanner.module.scss";
+
+export const SuggestionBanner = () => {
+    const seed = useMemo(() => Math.random(), []);
+    const routes = useRoutes();
+    const intl = useIntl();
+    const { isMobile } = useResponsive();
+
+    return (
+        <div className={styles.suggestionBannerContainer}>
+            <div className={cx(styles.suggestionBannerContext, { [styles.isMobile]: isMobile })}>
+                <div className={styles.suggestionBannerTitle}>
+                    <FormattedMessage id="suggestion.banner_title" defaultMessage="Suggest a debate question" />
+                </div>
+                <div className={styles.suggestionBannerDescription}>
+                    <FormattedMessage
+                        id="suggestion.banner_description"
+                        defaultMessage="Propose your own debate question and vote for your favourite. Questions that generate community interest are submitted to the editorial team."
+                    />
+                </div>
+                <Button
+                    to={routes.suggestionLocation.toUrl()}
+                    data-tid={"action_suggestions_banner"}
+                    className={cx(styles.suggestionBannerButton, { [styles.isMobile]: isMobile })}
+                    rightIcon={<Icon name="lightArrow" width={10} height={10} />}
+                >
+                    <span><FormattedMessage id="suggestion.suggest" defaultMessage="Suggest" /></span>
+                </Button>
+            </div>
+            <div className={styles.suggestionBannerList}>
+                <VotePaginatedList
+                    voteableType={"DebateSuggestion"}
+                    currentListId={"bannerSuggestionsList"}
+                    resource={"debate_suggestions"}
+                    resourcePropName="suggestion"
+                    filters={{ "is_expired": false, "is_accepted": false, "status": "accepted", "is_admin": false, "random": seed }}
+                    perPage={1}
+                    withPagination={false}
+                    searchBar={false}
+                    display={"column"}
+                    loadingComponent={<UserContentSkeleton />}
+                    emptyListComponent={
+                        <UserContentSkeleton border enableAnimation={false} numberLines={3}>
+                            <Button data-tid={"action_add_suggestion"} to={routes.suggestionLocation.toUrl()}>
+                                {intl.formatMessage({ id: "suggestion.suggestion_blank_box.add_suggestion", defaultMessage: "Add suggestion" })}
+                            </Button>
+                        </UserContentSkeleton>
+                    }
+                >
+                    <SuggestionBox />
+                </VotePaginatedList>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/suggestion/suggestion_banner/index.js
@@ -0,0 +1,1 @@
+export { SuggestionBanner } from "./SuggestionBanner"
