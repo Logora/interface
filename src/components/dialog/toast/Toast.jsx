d8fb08b629a4a7cb76a90620f52f7d95ae6f75f8
somayaassaadi <somaya.assaadi@gmail.com>
2024-11-26 14:22:28 +0100

    Added props toastTop and toastBottom to the Toast

--- src/components/dialog/toast/Toast.jsx
+++ src/components/dialog/toast/Toast.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import styles from './Toast.module.scss';
 
-export const Toast = ({ text, points, variant = "info", handleClose }) => {
+export const Toast = ({ text, points, variant = "info", handleClose, toastTop = '0px', toastBottom = '0px' }) => {
     return (
-        <div className={cx(styles.container, styles[variant])}>
+        <div className={cx(styles.container, styles[variant])} style={{ paddingTop: toastTop, paddingBottom: toastBottom }}>
             <div className={styles.body}>
                 <div className={styles.message}>
-                    { text }
+                    {text}
                 </div>
-                { points &&
+                {points &&
                     <div className={styles.points}>
                         <span className={styles.textPoints}>{points}</span>
                     </div>
@@ -27,9 +27,13 @@ Toast.propTypes = {
     /** Text of the toast */
     text: PropTypes.string.isRequired,
     /** Points earned displayed below the text */
-    points: PropTypes.oneOfType([PropTypes.string, PropTypes.number ]),
+    points: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     /** Type of the toast, can be 'info', 'success' or 'error' */
     variant: PropTypes.string,
     /** Callback triggered on close */
-    handleClose: PropTypes.func
+    handleClose: PropTypes.func,
+    /** Top padding for the toast */
+    toastTop: PropTypes.string,
+    /** Bottom padding for the toast */
+    toastBottom: PropTypes.string
 };
