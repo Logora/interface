e823647c09a78a2bd6a4084f82500db329e2c65e
Mboulgour <mboulgour@protonmail.com>
2025-01-17 12:00:27 +0100

    style: Remove unnecessary attributes from SVG paths in Chat and Clap icons

--- src/components/icons/bild_icons/Chat.jsx
+++ src/components/icons/bild_icons/Chat.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Chat = (props) => (
     <svg width="50" height="50" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
         <g clip-path="url(#clip0_323_1559)">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M2 4.47833H22V20.4844H10.7734L4 24.4783V20.4844H2V4.47833ZM4 18.4783H6V20.9809L10 18.4783H20V6.47833H4V18.4783Z" fill="#212529"/>
+            <path clip-rule="evenodd" d="M2 4.47833H22V20.4844H10.7734L4 24.4783V20.4844H2V4.47833ZM4 18.4783H6V20.9809L10 18.4783H20V6.47833H4V18.4783Z"/>
             <rect x="6" y="9.47833" width="12" height="2" fill="#159EA9"/>
             <rect x="6" y="13.4783" width="10" height="2" fill="#D00101"/>
         </g>
