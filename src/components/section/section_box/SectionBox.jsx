7dcaa4edfcb53b755a1438c504e622a7d8f82a71
somayaassaadi <somaya.assaadi@gmail.com>
2025-01-29 10:52:43 +0100

    [SectionBox]Move headerBox styles directly into SectionBox

--- src/components/section/section_box/SectionBox.jsx
+++ src/components/section/section_box/SectionBox.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './SectionBox.module.scss';
-import { SectionHeader } from '@logora/debate.section.section_header';
+
 
 export const SectionBox = (props) => {
   return (
-    <>
-      <div className={styles.sectionBoxContainer}>
-        <SectionHeader titleText={props.titleText} />
-        {props.children}
+    <div className={styles.sectionBoxContainer}>
+      <div className={styles.headerBox}>
+        {props.titleText}
       </div>
-    </>
-  )
+
+      {props.children}
+    </div>
+  );
 }
