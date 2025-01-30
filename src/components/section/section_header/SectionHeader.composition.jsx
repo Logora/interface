df63ece81595f0bc9e5300f8226f158cee513e24
Henry Boisgibault <h.boisgibault@gmail.com>
2025-01-30 09:51:57 +0100

    Remove SectionHeader component and related files and configs

--- src/components/section/section_header/SectionHeader.composition.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { SectionHeader } from './SectionHeader';
-
-export const DefaultSectionHeader = () => {
-    return <SectionHeader titleText="Section Title" />;
-};
