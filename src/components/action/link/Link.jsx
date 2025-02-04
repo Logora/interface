017f95ec3201036c097a0c520b66d1fc313aced2
Henry Boisgibault <h.boisgibault@gmail.com>
2025-02-04 10:28:58 +0100

    fix(Link.jsx): update import statement to use 'react-router-dom' for correct routing functionality

--- src/components/action/link/Link.jsx
+++ src/components/action/link/Link.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router';
+import { Link as RouterLink } from 'react-router-dom';
 import PropTypes from "prop-types";
 
 export const Link = ({ className, to, external, rel = "nofollow", target = "_self", children, ...rest }) => {
