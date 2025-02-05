b68c342aa095e04be6b04a4c4e6a32bbde89ad23
Henry Boisgibault <h.boisgibault@gmail.com>
2025-02-05 12:07:17 +0100

    refactor(useData): simplify function parameters by removing destructuring for better readability and consistency

--- src/components/data/data_provider/useData.jsx
+++ src/components/data/data_provider/useData.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { DataProviderContext } from "./DataProviderContext";
 
-export const useData = ({ dataKey, dataDefault }) => {
+export const useData = (dataKey, dataDefault) => {
     const { dataMap = {} } = useContext(DataProviderContext) || {}
     const [data, setData] = useState(dataKey in dataMap ? dataMap[dataKey] : dataDefault)
 
