af45cd07d920a2ab8f2b2cc776db179a3ac8f350
Henry Boisgibault <h.boisgibault@gmail.com>
2025-02-05 11:37:47 +0100

    fix(useData): ensure dataMap is always an object to prevent runtime errors when context is undefined

--- src/components/data/data_provider/useData.jsx
+++ src/components/data/data_provider/useData.jsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import { DataProviderContext } from "./DataProviderContext";
 
 export const useData = ({ dataKey, dataDefault }) => {
-    const { dataMap } = useContext(DataProviderContext)
+    const { dataMap = {} } = useContext(DataProviderContext) || {}
     const [data, setData] = useState(dataKey in dataMap ? dataMap[dataKey] : dataDefault)
 
     return [data, setData]
