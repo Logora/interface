83a5bacde0dd6c95a01c93fbe9100e8d87baf46e
Henry Boisgibault <h.boisgibault@gmail.com>
2025-02-06 15:49:28 +0100

    feat(ScrollToTop): enhance ScrollToTop component with navigation and smooth scrolling functionality refactor(ScrollToTop): prevent scrolling on initial render to improve user experience

--- src/components/tools/scroll_to_top/ScrollToTop.composition.jsx
+++ src/components/tools/scroll_to_top/ScrollToTop.composition.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import { ScrollToTop } from './ScrollToTop';
+
+const InnerComponent = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div onClick={() => navigate("?test=kjkj")}>Navigate</div>
+            <div id="test" style={{ backgroundColor: 'red', height: '200px', marginTop: '100vh', marginBottom: '100vh', scrollMarginTop: '10px' }}>
+                Scroll here
+            </div>
+            <ScrollToTop elementId={"test"} />
+        </>
+    )
+}
 
 export const DefaultScrollToTop = () => {
     return (
         <BrowserRouter>
-            <>
-                <div id="test" style={{ backgroundColor: 'red', height: '200px', marginTop: '700px' }}>Scroll here</div>
-                <ScrollToTop elementId={"test"} />
-            </>
+            <InnerComponent />
         </BrowserRouter>
     );
 };
--- src/components/tools/scroll_to_top/ScrollToTop.jsx
+++ src/components/tools/scroll_to_top/ScrollToTop.jsx
@@ -1,14 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router';
 
 export const ScrollToTop = ({ elementId }) => {
     const { pathname, search } = useLocation();
+    const firstRender = useRef(true);
 
     useEffect(() => {
-        if(typeof window !== 'undefined') {
-            const element = document.getElementById(elementId);
-            if (element) {
-                element.scrollIntoView({ behavior: 'smooth' });
+        if (typeof window !== 'undefined') {
+            if (!firstRender.current) {
+                const element = document.getElementById(elementId);
+                if (element) {
+                    element.scrollIntoView({ behavior: 'smooth' });
+                }
+            } else {
+                firstRender.current = false;
             }
         }
     }, [pathname, search]);
