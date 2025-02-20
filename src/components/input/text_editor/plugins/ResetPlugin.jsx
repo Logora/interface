963c2c53f4ae365bd89168d518da56208d184b87
Henry Boisgibault <h.boisgibault@gmail.com>
2025-02-20 16:07:40 +0100

    refactor(ResetPlugin): improve code readability by standardizing indentation and formatting for better maintainability

--- src/components/input/text_editor/plugins/ResetPlugin.jsx
+++ src/components/input/text_editor/plugins/ResetPlugin.jsx
@@ -1,38 +1,39 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useEffect } from "react";
-import { useInput } from '@logora/debate.input.input_provider';
-import { $getRoot, $createParagraphNode } from "lexical";
-import useSessionStorageState from '@rooks/use-sessionstorage-state';
+import { useInput } from "@logora/debate.input.input_provider";
+import { $getRoot, $createParagraphNode, $getSelection } from "lexical";
+import useSessionStorageState from "@rooks/use-sessionstorage-state";
 
 export const ResetPlugin = ({ storageUid }) => {
-    const [editor] = useLexicalComposerContext();
-    const { reset, setReset } = useInput();
-    const [content, setContent, removeContent] = useSessionStorageState(`TextEditor:content_${storageUid}`, {});
+	const [editor] = useLexicalComposerContext();
+	const { reset, setReset } = useInput();
+	const [content, setContent, removeContent] = useSessionStorageState(
+		`TextEditor:content_${storageUid}`,
+		{},
+	);
 
-    useEffect(() => {
-        if (reset) {
-            editor.update(() => {
-                const root = $getRoot();
-                root.clear();
-                const p = $createParagraphNode();
-                root.append(p);
-                removeContent();
+	useEffect(() => {
+		if (reset) {
+			editor.update(() => {
+				const root = $getRoot();
+				root.clear();
+				const p = $createParagraphNode();
+				root.append(p);
+				removeContent();
 
-                const rootElement = editor.getRootElement();
-                if (rootElement) {
-                    rootElement.blur();
-                    editor.update(() => {
-                        const selection = $getSelection();
-                        if (selection) {
-                            selection.clear();
-                        }
-                    });
-                }
+				const rootElement = editor.getRootElement();
+				if (rootElement) {
+					rootElement.blur();
+					const selection = $getSelection();
+					if (selection) {
+						selection.clear();
+					}
+				}
 
-                setReset(false);
-            });
-        }
-    }, [reset, editor, removeContent, setReset]);
+				setReset(false);
+			});
+		}
+	}, [reset, editor, removeContent, setReset]);
 
-    return null;
+	return null;
 };
