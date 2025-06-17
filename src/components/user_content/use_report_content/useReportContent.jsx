18714ac2d4307eb60a6a9575705861a5fbb3d339
somayaassaadi <somaya.assaadi@gmail.com>
2025-06-17 14:26:13 +0200

    feat(useReportContent): integrate useConfig to allow anonymous user reporting option in ReportModal

--- src/components/user_content/use_report_content/useReportContent.jsx
+++ src/components/user_content/use_report_content/useReportContent.jsx
@@ -2,11 +2,13 @@ import React, { Suspense } from 'react';
 import { useAuth } from "@logora/debate.auth.use_auth";
 import { useModal } from '@logora/debate.dialog.modal';
 import { useAuthRequired } from '@logora/debate.hooks.use_auth_required';
+import { useConfig } from '@logora/debate.data.config_provider';
 import { ReportModal } from './ReportModal';
 
 export const useReportContent = (reportableType, reportableId, modalTitle) => {
     const { isLoggedIn } = useAuth();
     const { showModal } = useModal();
+	const config = useConfig();
 	const requireAuthentication = useAuthRequired();
 
     const reportContent = () => {
@@ -17,6 +19,7 @@ export const useReportContent = (reportableType, reportableId, modalTitle) => {
 						reportableType={reportableType}
 						reportableId={reportableId}
 						title={modalTitle}
+						allowAnonymousUser={config.actions?.allowAnonymousUser}
 					/>
 				</Suspense>
 			);
