0e78299b8dbab9fb1f63150ac04b4457cc5bdfa5
somayaassaadi <somaya.assaadi@gmail.com>
2025-08-04 17:29:13 +0200

    feat(SearchInput): wrap SearchInput components in IntlProvider for internationalization support feat(SearchInput): add aria-labels for accessibility to input and submit icon test(SearchInput): update tests to include IntlProvider for consistent rendering with internationalization

--- src/components/input/search_input/SearchInput.composition.jsx
+++ src/components/input/search_input/SearchInput.composition.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
+import { IntlProvider } from 'react-intl';
 import { SearchInput } from './SearchInput';
 import { IconProvider } from '@logora/debate.icons.icon_provider';
 import * as regularIcons from '@logora/debate.icons.regular_icons';
 
 export const DefaultSearchInput = () => {
     return (
-        <IconProvider library={regularIcons}>
-            <SearchInput onSearchSubmit={(query) => alert(query)} placeholder={"Search"} />
-        </IconProvider>
+        <IntlProvider locale="en">
+            <IconProvider library={regularIcons}>
+                <SearchInput onSearchSubmit={(query) => alert(query)} placeholder={"Search"} />
+            </IconProvider>
+        </IntlProvider>
     )
 };
 
 export const ReducedByDefaultSearchInput = () => {
     return (
-        <IconProvider library={regularIcons}>
-            <SearchInput onSearchSubmit={() => alert("SearchInput callback")} placeholder={"Search"} reducedByDefault={true} />
-        </IconProvider>
+        <IntlProvider locale="en">
+            <IconProvider library={regularIcons}>
+                <SearchInput onSearchSubmit={() => alert("SearchInput callback")} placeholder={"Search"} reducedByDefault={true} />
+            </IconProvider>
+        </IntlProvider>
     )
 };
 
 export const DisabledSearchInput = () => {
     return (
-        <IconProvider library={regularIcons}>
-            <SearchInput onSearchSubmit={() => alert("SearchInput callback")} placeholder={"Search"} disabled={true} />
-        </IconProvider>
+        <IntlProvider locale="en">
+            <IconProvider library={regularIcons}>
+                <SearchInput onSearchSubmit={() => alert("SearchInput callback")} placeholder={"Search"} disabled={true} />
+            </IconProvider>
+        </IntlProvider>
     )
+
 };
