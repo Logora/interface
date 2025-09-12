bb819628087ab6843c3dbdeb47cfbe34984faf30
somayaassaadi <somaya.assaadi@gmail.com>
2025-09-12 16:38:40 +0200

    fix(icons): update viewBox attribute in Alarm component for consistent sizing across hero and krone icons

--- src/components/icons/krone_icons/Alarm.jsx
+++ src/components/icons/krone_icons/Alarm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Alarm = (props) => (
     <svg
         {...props} 
-        viewBox="4 4 27 17"
+        viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
     >
