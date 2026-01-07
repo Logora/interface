dbc87c4fec3f43e4f6a5c1332162c749334b5952
somayaassaadi <somaya.assaadi@gmail.com>
2026-01-07 15:19:08 +0100

    fix(Modal.jsx): add aria-modal attribute to modal dialog for improved accessibility

--- src/components/dialog/modal/Modal.jsx
+++ src/components/dialog/modal/Modal.jsx
@@ -23,7 +23,7 @@ export const Modal = ({ title, showCloseButton = false, fullScreen, children, di
 
   return (
     <div className={cx(styles.modalWrapper, { [styles.modalWrapperFullScreen]: fullScreen })}>
-      <div className={cx(styles.modalDialog, { [styles.modalDialogFullScreen]: fullScreen })} role="dialog" {...rest}>
+      <div className={cx(styles.modalDialog, { [styles.modalDialogFullScreen]: fullScreen })} role="dialog"  aria-modal="true" {...rest}>
         <div className={cx(styles.modalContainer)} ref={modalRef}>
           <div className={cx(styles.modalHeader, { [styles.modalHeaderWithTitle]: title })}>
             {title &&
